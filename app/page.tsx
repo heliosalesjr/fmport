1f7099bcdfde01d2f9747285fa2441ba16a8df61
heliosalesjr <65938536+heliosalesjr@users.noreply.github.com>
2024-07-29 14:19:04 -0300

    floating navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,18 @@
+import { FloatingNav } from "@/components/FloatingNav";
 import Hero from "@/components/Hero";
-import Image from "next/image";
+import { FaHome, FaUser, FaProjectDiagram, FaEnvelope } from "react-icons/fa";
 
 export default function Home() {
   return (
     <>
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
+      <FloatingNav navItems={ [
+        { name: "Home", link: "#", icon: <FaHome /> },
+        { name: "About", link: "#about", icon: <FaUser /> },
+        { name: "Projects", link: "#projects", icon: <FaProjectDiagram /> },
+        { name: "Contact", link: "#contact", icon: <FaEnvelope /> },
+      ]} />
        <Hero />
       </div>
     </main>

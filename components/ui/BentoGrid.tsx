0089440d7f6e8c4bbbf925bcb2f427502824e8c5
heliosalesjr <65938536+heliosalesjr@users.noreply.github.com>
2024-07-30 13:41:10 -0300

    gradients adjustments

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -82,9 +82,9 @@ export const BentoGridItem = ({
       style={{
         //   add these two
         //   you can generate the color from here https://cssgradient.io/
-        background: "rgb(4,7,29)",
+        background: "rgb(2,0,36)",
         backgroundColor:
-          "linear-gradient(90deg, rgba(4,7,29,1) 0%, rgba(12,14,35,1) 100%)",
+          "linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(3,153,0,1) 41%, rgba(41,9,121,1) 100%)",
       }}
     >
       {/* add img divs */}
